7e6ce328cc67f399a7decbbc8880b4b81587b245
Angelina <angie1508nz@gmail.com>
2026-04-23 20:33:51 +1200

    Add files via upload

---
+++ TraitType.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Assignment1;
+
+/**
+ *
+ * @author angie
+ */
+public enum TraitType {
+    BRAVE,
+    CUNNING,
+    TIMID
+}
