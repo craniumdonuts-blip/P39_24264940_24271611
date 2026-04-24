6ddd94848ee3f9fb2d0d40b209e3ad010f6e17ca
craniumdonuts-blip <craniumdonuts@gmail.com>
2026-04-24 19:22:11 +1200

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,53 +1,50 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Assignment1;
+package project1;
 
 /**
  *
  * @author Angie
  */
 public class Player {
+
     private String name;
     private TraitType trait;
     private int totalPoints;
     private Inventory inventory;
-    
+
     // constructor 
-    public Player(){
+    public Player() {
         this.name = "";
         this.totalPoints = 0;
         this.inventory = new Inventory();
     }
-    
+
     // getters and setters
-    public String getName(){
+    public String getName() {
         return name;
     }
-    
-    public void setName(String name){
+
+    public void setName(String name) {
         this.name = name;
     }
-    
-    public TraitType getTrait(){
+
+    public TraitType getTrait() {
         return trait;
     }
-    
-    public void setTrait(TraitType trait){
+
+    public void setTrait(TraitType trait) {
         this.trait = trait;
     }
-    
-    public int getTotalPoints(){
+
+    public int getTotalPoints() {
         return totalPoints;
     }
-    
+
     // be able to plus/minus total points
-    public void changeTotalPoints(int points){
+    public void changeTotalPoints(int points) {
         totalPoints += points;
     }
-    
-    public Inventory getInventory(){
+
+    public Inventory getInventory() {
         return inventory;
     }
 
